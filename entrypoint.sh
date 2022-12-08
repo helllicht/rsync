e5b39f6f14020ba0f90bbcbab43309792166deb0
Moses Otten <otten@helllicht.com>
2022-12-08 19:34:53 +0100

    Add sudo to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -67,10 +67,10 @@ if [ "$AUTHORIZATION_METHOD" = "PASSWORD" ]; then
   SSH_KEY_SWITCH=
 else
   # create private key file
-  mkdir -p ~/.ssh
-  touch ~/.ssh/private_key
+  sudo mkdir -p ~/.ssh
+  sudo touch ~/.ssh/private_key
   echo "$PRIVATE_KEY" >~/.ssh/id_rsa
-  chmod 600 ~/.ssh/private_key
+  sudo chmod 600 ~/.ssh/private_key
 
   ADDITIONAL_PACKAGES=
   SSHPASS_COMMAND=
@@ -93,8 +93,8 @@ echo "Installing packages: rsync ssh $ADDITIONAL_PACKAGES ..."
 echo ""
 
 # install packages
-apt-get update
-apt-get install rsync ssh $ADDITIONAL_PACKAGES -y
+sudo apt-get update
+sudo apt-get install rsync ssh $ADDITIONAL_PACKAGES -y
 
 echo ""
 echo 'Installed ✅'
@@ -137,6 +137,6 @@ fi
 
 echo "Running rsync..."
 echo ""
-$SSHPASS_COMMAND rsync -avzr$DRY_SWITCH $DELETE_FLAG --exclude-from="$EXCLUDE_FILE" --rsh="ssh -o StrictHostKeyChecking=no $PORT_SWITCH" $LOCAL_DIRECTORY $REMOTE_USER@$REMOTE_SERVER:$REMOTE_DIRECTORY
+sudo $SSHPASS_COMMAND rsync -avzr$DRY_SWITCH $DELETE_FLAG --exclude-from="$EXCLUDE_FILE" --rsh="ssh -o StrictHostKeyChecking=no $PORT_SWITCH" $LOCAL_DIRECTORY $REMOTE_USER@$REMOTE_SERVER:$REMOTE_DIRECTORY
 echo ""
 echo "Done ✅"
