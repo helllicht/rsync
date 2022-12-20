2510c155635ad9fc2f4e5e199c499789e7162a23
Moses Otten <otten@helllicht.com>
2022-12-20 16:23:35 +0100

    Fix quoting

--- entrypoint.sh
+++ entrypoint.sh
@@ -79,7 +79,7 @@ if [[ $AUTHORIZATION_METHOD = "PASSWORD" ]]; then
   ADDITIONAL_PACKAGES=sshpass
 
   # add sshpass command to authorize by password
-  SSHPASS_COMMAND= 'sshpass -p "' + $REMOTE_PASSWORD + '"'
+  SSHPASS_COMMAND='sshpass -p "' + $REMOTE_PASSWORD + '"'
 else
   # create private key file
   sudo mkdir -p ~/.ssh
